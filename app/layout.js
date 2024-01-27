43f0793406a11033279a2df0a1ee132f3e4ddc7b
francisekkagit <francisxekka@gmail.com>
2024-01-27 15:10:54 -0800

    Theme Test 2

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,10 @@ export default function RootLayout({ children }) {
       <body className='max-container hide-scrollbar'>
         <NextUIProvider>
           {/* Dark Default */}
+          <NextThemesProvider attribute='class' defaultTheme='dark'>
             <Navbar/>
             {children}
+          </NextThemesProvider>
         </NextUIProvider>
           
   
