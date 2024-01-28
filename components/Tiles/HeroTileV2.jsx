b1dcdadb95d1a1a81365c45500e593be2387519b
francisekkagit <francisxekka@gmail.com>
2024-01-27 16:08:37 -0800

    Hero Alignment

--- components/Tiles/HeroTileV2.jsx
+++ components/Tiles/HeroTileV2.jsx
@@ -5,13 +5,13 @@ const HeroTileV2 = () => {
   return (
     <div className='flex flex-col md:flex-row'>
         
-        <div className='border-2 border-red-500 w-full h-[50vh] md:w-[60vw] md:h-screenNav'>
+        <div className='w-full h-[40vh] md:w-[60vw] md:h-screenNav flex flex-col items-center justify-center text-center pl-4 pr-2'>
           <h3>Hi, I&#39;m Francis</h3>
-          <h1 className='regular-64'>Fullstack Developer</h1>
+          <h1 className='regular-40 md:regular-64'>Fullstack Developer</h1>
           <h3>Located in Danville, CA</h3>
         </div>
-        <div className='border-2 border-green-500 w-full h-[43.6vh] md:w-[40vw] md:h-screenNav'>
-          <Image src="/component_imgs/francisProfile1.png" alt='Francis Ekka' width={400} height={400} className='drop-shadow-2xl'/>
+        <div className='w-full md:w-[40vw] md:h-screenNav overflow-hidden flex items-center justify-center pr-4 pl-2'>
+          <Image src="/component_imgs/francisProfile1.png" alt='Francis Ekka' width={350} height={350} className='drop-shadow-2xl'/>
         </div>
         
     </div>
