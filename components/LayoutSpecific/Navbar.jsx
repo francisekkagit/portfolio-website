4fc77e9d4e0af01b2e0bc9da9dbd8645170ccbb3
francisekkagit <francisxekka@gmail.com>
2024-01-29 17:33:56 -0800

    Theme Switcher on Mobile Nav

--- components/LayoutSpecific/Navbar.jsx
+++ components/LayoutSpecific/Navbar.jsx
@@ -48,7 +48,8 @@ const Navbar = () => {
         
 
         {/* Dropdown Menu Mobile*/}
-        <div className='sm:hidden'>
+        <div className='sm:hidden rowCenter gap-4'>
+            <ThemeSwitcher/>
             <Dropdown backdrop="blur" className='bg-website-20'>
                 <DropdownTrigger>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
