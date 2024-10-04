782383103b7c1f3f9ae35a2c71c3587bb870c270
francisekkagit <francisxekka@gmail.com>
2024-10-04 02:52:37 -0700

    spacing

--- components/Tiles/Body/UnderConstruction.jsx
+++ components/Tiles/Body/UnderConstruction.jsx
@@ -15,14 +15,15 @@ const UnderConstruction = () => {
         <div className='RedHat border-1 border-border_tile bg-background_tile w-full sm:w-[50%] rounded-xl shadow-xl flex flex-col items-center gap-3 p-3 sm:gap-5 sm:p-5'>
             <img src='/component_imgs/francisProfile1.png' width={2979} height={2979} className='w-full max-w-[450px] rounded-full shadow-lg'/>
 
-            <p className='textShadow text-[16px]'>Full Stack Developer in Danville, CA</p>
-
-            <a className='flex gap-1 items-center hover:bg-background hover:shadow-md ease-in-out duration-150 p-2 rounded-xl' href="mailto: francisxekka@gmail.com">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25" />
-              </svg>
-              <p>francisxekka@gmail.com</p>
-            </a>
+            <div className='flex flex-col gap-2'>
+              <p className='textShadow text-[16px]'>Full Stack Developer in Danville, CA</p>
+              <a className='flex gap-1 justify-center hover:bg-background hover:shadow-md ease-in-out duration-150 p-2 rounded-xl' href="mailto: francisxekka@gmail.com">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25" />
+                </svg>
+                <p className=''>francisxekka@gmail.com</p>
+              </a>
+            </div>
         
         </div>
     </div>
