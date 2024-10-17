cf3afad9316460fb3fbbdb822e9155ad0f6568d0
francisekkagit <francisxekka@gmail.com>
2024-10-17 00:42:02 -0700

    Git Ignore, LinkSizing

--- components/Tiles/Child/EducationList.jsx
+++ components/Tiles/Child/EducationList.jsx
@@ -11,10 +11,10 @@ const EducationList = () => {
                 <Image src='/avatars/asuLogo.png' width={3062} height={2160} alt='soemthig' className='w-[64px]'/>
                 <div className='flex flex-col leading-0 justify-around gap-1'>
                     <span className='font-semibold text-[16px]'>Arizona State University</span>
-                    <span className='text-subtileText text-[14px]'>2018 - 2022</span>
+                    <span className='text-subtileText font-light text-[14px]'>2018 - 2022</span>
                 </div>
             </div>
-            <div className='flex flex-col gap-2 text-subtileText text-center'>
+            <div className='flex flex-col gap-2 text-subtileText font-light text-center'>
                 <span>Ira A. Fulton Schools of Enineering</span>
                 <span>BS in Computer Science</span>
                 <span>GPA 3.48 Cum Laude</span>
@@ -26,19 +26,21 @@ const EducationList = () => {
                 <Image src='/avatars/awsIcon.png' width={512} height={512} alt='soemthig' className='w-[64px]'/>
                 <div className='flex flex-col leading-0 justify-around gap-1'>
                     <span className='font-semibold text-[16px]'>Amazon Web Services</span>
-                    <span className='text-subtileText text-[14px]'>April 2023</span>
+                    <span className='text-subtileText font-light text-[14px]'>April 2023</span>
                 </div>
             </div>
-            <div className='flex flex-col leading-none gap-2 text-subtileText text-center'>
+            <div className='flex flex-col leading-none gap-2 text-subtileText font-light text-center'>
                 <span>Certified AWS Cloud Practitioner</span>
                 <span className='flex justify-center items-center gap-2'><span className='whitespace-nowrap'>Credential ID</span><Snippet size='sm' symbol='' className=''>N4BLKEQ14JV41VGZ</Snippet></span>
             </div>
-            <a href='https://aws.amazon.com/verification' className='flex gap-1 items-center justify-center text-blue-500 cursor-pointer text-[14px]'>
-                <span>Validate my certificate</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-                </svg>
-            </a>
+            <div className='flex justify-center'>
+                <a href='https://aws.amazon.com/verification' className='flex gap-1 items-center justify-center text-blue-500 cursor-pointer text-[14px]'>
+                    <span>Validate my certificate</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                    </svg>
+                </a>
+            </div>
         </div>
 
         <div className='bg-subtile p-3 lg:p-5 flex flex-col gap-3 rounded-xl shadow-md leading-none'>
@@ -46,19 +48,21 @@ const EducationList = () => {
                 <Image src='/avatars/dlLogo.png' width={512} height={512} alt='soemthig' className='w-[64px]'/>
                 <div className='flex flex-col leading-none justify-around gap-1'>
                     <span className='font-semibold text-[16px]'>DeepLearning.AI</span>
-                    <span className='text-subtileText text-[14px]'>September 2024</span>
+                    <span className='text-subtileText font-light text-[14px]'>September 2024</span>
                 </div>
             </div>
-            <div className='flex flex-col leading-none gap-2 text-subtileText text-center'>
+            <div className='flex flex-col leading-none gap-2 text-subtileText font-light text-center'>
                 <span>TensorFlow Professional Developer</span>
                 <span>Credential ID 7MFYEEGDUM83</span>
             </div>
-            <a href='https://www.coursera.org/account/accomplishments/professional-cert/7MFYEEGDUM83' className='flex gap-1 items-center justify-center text-blue-500 cursor-pointer text-[14px]'>
-                <span>Validate my certificate</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-                </svg>
-            </a>
+            <div className='flex justify-center'>
+                <a href='https://www.coursera.org/account/accomplishments/professional-cert/7MFYEEGDUM83' className='flex gap-1 items-center justify-center text-blue-500 cursor-pointer text-[14px]'>
+                    <span>Validate my certificate</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                    </svg>
+                </a>
+            </div>
         </div>
     </div>
   )
--- components/Tiles/Child/EmploymentList.jsx
+++ components/Tiles/Child/EmploymentList.jsx
@@ -17,20 +17,20 @@ const EmploymentList = () => {
                 <Image src='/avatars/diabloLogo.png' width={2291} height={2291} alt='soemthig' className='w-[64px]'/>
                 <div className='flex flex-col leading-none justify-around items-start'>
                     <span className='text-[16px] font-semibold'>Full Stack Developer</span>
-                    <span className='text-sm text-subtileText'>June 2023 - Present</span>
-                    <span className='text-sm text-subtileText link_normal'>Diablo Escapes</span>
+                    <span className='text-sm text-subtileText font-light'>June 2023 - Present</span>
+                    <span className='text-sm text-subtileText font-light link_normal'>Diablo Escapes</span>
                     
                 </div>
             </div>
-            <div className='flex flex-col gap-2 relative text-subtileText'>
-              <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Design, develop, and maintain an e-commerce application using Next.js, TailwindCSS, Node.js, and MongoDB ensuring security and compliance with industry standards.</span></div>
-              <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Build and manage multiple responsive MERN applications.</span></div>
-              <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Implement CI/CD pipelines using GitHub, ensuring seamless updates and integrations.</span></div>
+            <div className='flex flex-col gap-2 relative text-subtileText font-light'>
+              <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Design, develop, and maintain an e-commerce application using <span className='font-normal'>Next.js, TailwindCSS, Node.js, and MongoDB</span> ensuring security and compliance with industry standards.</span></div>
+              <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Build and manage multiple <span className='font-normal'>responsive MERN applications</span>.</span></div>
+              <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Implement <span className='font-normal'>CI/CD pipelines using GitHub</span>, ensuring seamless updates and integrations.</span></div>
               {!showDE && (<div className='max-md:hidden h-4'></div>)}
               {showDE && (
                 <div className='flex flex-col gap-2'>
-                  <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Enhance Search Engine Optimization (SEO) using tools such as Google Search Console and Google Analytics to monitor performance, analyze traffic data, track user behavior, and increase website traffic.</span></div>
-                  <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Head marketing efforts using Google Ads. Manage Google Ad campaigns and collaborate with cross-functional teams to increase brand awareness.</span></div>
+                  <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Enhance Search Engine Optimization (SEO) using tools such as <span className='font-normal'>Google Search Console</span> and <span className='font-normal'>Google Analytics</span> to monitor performance, analyze traffic data, track user behavior, and increase website traffic.</span></div>
+                  <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Head marketing efforts using <span className='font-normal'>Google Ads</span>. Manage Google Ad campaigns and collaborate with cross-functional teams to increase brand awareness.</span></div>
                 </div>
               )}
               {!showDE && (
@@ -46,12 +46,14 @@ const EmploymentList = () => {
               )}
             </div>
             {showDE && (
-                <a href='https://www.diabloescapes.com/' className='flex gap-1 items-center text-blue-500 cursor-pointer text-[14px]' target='_blank'>
-                  <span>Visit diabloescapes.com</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-                  </svg>
-                </a>
+                <div className='flex'>
+                  <a href='https://www.diabloescapes.com/' className='flex gap-1 items-center text-blue-500 cursor-pointer text-[14px] border-2' target='_blank'>
+                    <span>Visit diabloescapes.com</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                    </svg>
+                  </a>
+                </div>
               )}
         </div>
 
@@ -60,12 +62,12 @@ const EmploymentList = () => {
             <div className='w-16 h-16 bg-gray-400 rounded-xl'></div>
                 <div className='flex flex-col leading-none justify-around items-start'>
                     <span className='text-[16px] font-semibold'>Web Development Intern</span>
-                    <span className='text-sm text-subtileText'>December 2022 - June 2023</span>
-                    <span className='text-sm text-subtileText link_normal'>Eminence Design & Solutions</span>
+                    <span className='text-sm text-subtileText font-light'>December 2022 - June 2023</span>
+                    <span className='text-sm text-subtileText font-light link_normal'>Eminence Design & Solutions</span>
                     
                 </div>
             </div>
-            <div className='flex flex-col gap-2 relative text-subtileText'>
+            <div className='flex flex-col gap-2 relative text-subtileText font-light'>
               <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Led the development of a React.js service portal, integrating PostgreSQL and MongoDB databases.</span></div>
               <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Strengthened web development fundamentals through hands-on experience with HTML, CSS, and Javascript.</span></div>
               <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Participated in Agile development practices including stand-ups, sprint planning, and continuous integration.</span></div>
@@ -93,12 +95,12 @@ const EmploymentList = () => {
                 <Image src='/avatars/bsdxr.jpeg' width={200} height={200} alt='soemthig' className='w-[64px]'/>
                 <div className='flex flex-col leading-none justify-around items-start'>
                     <span className='text-[16px] font-semibold'>Capstone Machine Learning Developer</span>
-                    <span className='text-sm text-subtileText'>May 2022 - December 2022</span>
-                    <span className='text-sm text-subtileText link_normal'>Bit Space Development</span>
+                    <span className='text-sm text-subtileText font-light'>May 2022 - December 2022</span>
+                    <span className='text-sm text-subtileText font-light link_normal'>Bit Space Development</span>
                     
                 </div>
             </div>
-            <div className='flex flex-col gap-2 relative text-subtileText'>
+            <div className='flex flex-col gap-2 relative text-subtileText font-light'>
               <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Completion of the BSDXR Computer Vision Project.</span></div>
               <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Contributed to the AI Model Training initiative using NVIDIA Jetson Nanos, utilizing live video feeds captured by CSI Cameras to enhance computer vision capabilities.</span></div>
               <div className='flex gap-2'><div className='bulletPoint bg-current'/><span>Successfully completed Machine Learning certifications through NVIDIA’s Deep Learning Institute, specializing in Model Building with Image Classification.</span></div>
@@ -120,12 +122,14 @@ const EmploymentList = () => {
               )}
             </div>
             {showBSD && (
-                <a href='https://www.bitspacedevelopment.com/' className='flex gap-1 items-center text-blue-500 cursor-pointer text-[14px]' target='_blank'>
-                  <span>bitspacedevelopment.com</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-                  </svg>
-                </a>
+                <div className='flex'>
+                  <a href='https://www.bitspacedevelopment.com/' className='flex gap-1 items-center text-blue-500 cursor-pointer text-[14px] border-2' target='_blank'>
+                    <span>bitspacedevelopment.com</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                    </svg>
+                  </a>
+                </div>
               )}
         </div>
         
