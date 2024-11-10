614381072f8e829d6d6b268a900220d58d041693
francisekkagit <francisxekka@gmail.com>
2024-11-09 16:50:17 -0800

    Google Tag Manager

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,15 @@ import Navbar from '@/components/LayoutSpecific/Navbar'
 import {NextUIProvider} from "@nextui-org/react";
 import {ThemeProvider as NextThemesProvider} from "next-themes";
 import Footer from '@/components/LayoutSpecific/Footer';
+import { GoogleTagManager } from '@next/third-parties/google';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <GoogleTagManager gtmId="GTM-M4ZZPB96"/>
       <body className='max-container hide-scrollbar'>
         <NextUIProvider>
           {/* Dark Default */}
